fbe37a835f985b6f8a48331040a8eea16564ffa1
vatsalsaglani <saglanivatsal@gmail.com>
2023-05-01 19:21:14 +0530

    OpenAI Configs

---
+++ configs.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_URL = os.environ.get("OPENAI_API_URL",
+                                "https://api.openai.com/v1/embeddings")
+OPENAI_EMBEDDING_MODEL = os.environ.get("OPENAI_EMBEDDING_MODEL",
+                                        "text-embedding-ada-002")
+OPENAI_CHAT_COMPLETION_URL = os.environ.get(
+    "OPENAI_CHAT_COMPLETION_API", "https://api.openai.com/v1/chat/completions")
+OPENAI_CHAT_COMPLETION_MODEL = os.environ.get("OPENAI_CHAT_COMPLETION_MODEL",
+                                              "gpt-3.5-turbo")
